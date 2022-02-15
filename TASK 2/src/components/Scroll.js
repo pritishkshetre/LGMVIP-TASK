95ab42dc4dc5f4bfcb959954dd1f289d112a32a0
Pritish Kshetre <pritishkshetre@gmail.com>
2022-02-15 23:08:33 +0530

    Add files via upload

---
+++ TASK 2/src/components/Scroll.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return(
+        
+        <div style={{height: 'auto'}}>
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll;
+
+
+//PRITISH KSHETRE
